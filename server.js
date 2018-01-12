d06b51f5ba9c47d39c95e786d584f50d6f5e9f80
stephen <stephenrs7@gmail.com>
2018-01-11 19:47:00 -0800

    first

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
